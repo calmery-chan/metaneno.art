e3132cfaf440fcdffa53b04f6963305d091ee2e9
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-08-02 05:54:25 +0900

    swr を追加、/api/contentful/works からデータを取得する

--- src/pages/preview.tsx
+++ src/pages/preview.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import useSWR from "swr";
 import { withBasicAuth } from "~/utils/with-basic-auth";
+import { WorkCollection } from "~/types/contentful";
 
-const Preview = () => <div>{process.env.NODE_ENV}</div>;
+const path = "/api/contentful/works";
+const fetcher = (): Promise<WorkCollection> =>
+  fetch(path).then((r) => r.json());
+
+const Preview = () => {
+  const { data, error } = useSWR<WorkCollection>(path, fetcher);
+
+  if (error) {
+    return <div>Error</div>;
+  }
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return <div>{data.data.worksCollection.total}</div>;
+};
 
 export default Preview;
 export const getServerSideProps = withBasicAuth();
