4dd1b56973b517d5443dda14b893f46c8252593f
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-08-16 02:21:03 +0900

    テーブルのタイトルにソートに使うアイコンを追加する

--- src/pages/admin/serial_codes.tsx
+++ src/pages/admin/serial_codes.tsx
@@ -18,6 +18,32 @@ const getSerialCodes = (): Promise<{ data: SerialCode[] }> => {
   }).then((r) => r.json());
 };
 
+const formatSerialCode = (serialCode: string) => {
+  if (serialCode.length !== 16) {
+    return serialCode;
+  }
+
+  return [...serialCode.matchAll(/\w{4}/g)].map((s) => s[0]).join("-");
+};
+
+// Components
+
+const SortableColumnTitle: React.FC = ({ children }) => (
+  <div className="cursor-pointer flex items-center">
+    {children}
+    <svg
+      className="ml-auto fill-current"
+      width="14"
+      height="14"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M15.5 11.4545C15.5 11.6761 15.4196 11.8679 15.2588 12.0298L9.57129 17.7571C9.41048 17.919 9.22005 18 9 18C8.77995 18 8.58952 17.919 8.42871 17.7571L2.74121 12.0298C2.5804 11.8679 2.5 11.6761 2.5 11.4545C2.5 11.233 2.5804 11.0412 2.74121 10.8793C2.90202 10.7173 3.09245 10.6364 3.3125 10.6364H14.6875C14.9076 10.6364 15.098 10.7173 15.2588 10.8793C15.4196 11.0412 15.5 11.233 15.5 11.4545ZM15.5 6.54545C15.5 6.76705 15.4196 6.95881 15.2588 7.12074C15.098 7.28267 14.9076 7.36364 14.6875 7.36364H3.3125C3.09245 7.36364 2.90202 7.28267 2.74121 7.12074C2.5804 6.95881 2.5 6.76705 2.5 6.54545C2.5 6.32386 2.5804 6.1321 2.74121 5.97017L8.42871 0.242898C8.58952 0.0809659 8.77995 0 9 0C9.22005 0 9.41048 0.0809659 9.57129 0.242898L15.2588 5.97017C15.4196 6.1321 15.5 6.32386 15.5 6.54545Z" />
+    </svg>
+  </div>
+);
+
 // Types
 
 type SerialCode = {
@@ -51,9 +77,15 @@ const SerialCodes: React.FC = () => {
           <Thead>
             <Tr>
               <Th>シリアルコード</Th>
-              <Th>公開状態</Th>
-              <Th>作成日時</Th>
-              <Th>最終更新日時</Th>
+              <Th>
+                <SortableColumnTitle>公開状態</SortableColumnTitle>
+              </Th>
+              <Th>
+                <SortableColumnTitle>作成日時</SortableColumnTitle>
+              </Th>
+              <Th>
+                <SortableColumnTitle>最終更新日</SortableColumnTitle>
+              </Th>
             </Tr>
           </Thead>
           <Tbody>
@@ -61,7 +93,7 @@ const SerialCodes: React.FC = () => {
               <Tr key={key}>
                 <Td>
                   <p className="text-gray-900 whitespace-no-wrap">
-                    {serialCode.serial_code}
+                    {formatSerialCode(serialCode.serial_code)}
                   </p>
                 </Td>
                 <Td>
