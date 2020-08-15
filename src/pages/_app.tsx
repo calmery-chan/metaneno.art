4a8a42d8687132fdac23dc367ca40b43afbcc956
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-08-15 22:27:49 +0900

    CSS の場所を変更する

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { AppProps } from "next/app";
-import "./styles.css";
+import "~/styles/index.css";
 
 const App = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
