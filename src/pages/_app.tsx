98ed379fb83ce9e7a7eff44996ffa4148d835bb0
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-08-15 15:04:44 +0900

    Tailwind CSS を使えるようにする

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { AppProps } from "next/app";
+import "./styles.css";
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
+
+export default App;
