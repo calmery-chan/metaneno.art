bd9cfe951b08aa69ec9529c536c7e37240206d7e
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-08-15 23:23:19 +0900

    styled-components の createGlobalStyle を使えるようにする

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import NextApp from "next/app";
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+class App extends NextApp {
+  public render = () => {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <>
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </>
+    );
+  };
+}
+
+export default App;
