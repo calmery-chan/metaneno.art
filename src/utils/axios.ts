11e94505757cbb5e79cfdeeec139e1b0b34a5abd
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-08-20 01:15:17 +0900

    API サーバの URL を修正する

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://metaneno.vercel.app/"
+      ? "https://metaneno.herokuapp.com/"
       : "http://localhost:5000/",
   headers: {
     Accept: "application/json",
