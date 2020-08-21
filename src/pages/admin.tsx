b69df518df30cee7bd7f72493833f36cbcd6db5a
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-08-21 18:06:41 +0900

    admin.tsx に /admin 以下へのリンクを追加する

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -4,7 +4,22 @@ import React from "react";
 import { withAdmin } from "~/utils/with-admin";
 
 const Admin: NextPage = () => {
-  return <Link href="/admin/serial_codes">Serial Codes</Link>;
+  return (
+    <>
+      <div>
+        <Link href="/admin/serial_codes">Serial Codes</Link>
+      </div>
+      <div>
+        <Link href="/admin/canvas">Canvas</Link>
+      </div>
+      <div>
+        <Link href="/admin/works">Works</Link>
+      </div>
+      <div>
+        <Link href="/admin/multiplay">Multiplay</Link>
+      </div>
+    </>
+  );
 };
 
 export default withAdmin(Admin);
