e4be69ceb0bb5872cce093f18ceb477f464a3666
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-10-04 05:10:28 +0900

    css を scss に置き換える

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import React from "react";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
