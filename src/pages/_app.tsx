6cd08d0af7dafb93d6cb91cd3e0aa9fbb60b0031
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-10-20 23:26:37 +0900

    next-seo を追加、OGP 設定の準備をする

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
 import React from "react";
 import "~/styles/globals.scss";
 
-const App = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
-};
+const App = ({ Component, pageProps }: AppProps): JSX.Element => (
+  <>
+    <Component {...pageProps} />
+    <DefaultSeo {...require("~/utils/next-seo")} />
+  </>
+);
 
 export default App;
