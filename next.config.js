b4a6508ba968f1d6f21f47c56fb296c69aad0f5e
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-10-27 00:17:59 +0900

    @zeit/next-source-maps を追加、ソースマップを出力する

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const nextBundleAnaluzer = require("@next/bundle-analyzer");
+const withSourceMaps = require("@zeit/next-source-maps");
 const { nextI18NextRewrites } = require("next-i18next/rewrites");
 
 const localSubpaths = {
@@ -11,12 +12,14 @@ const withBundleAnalyzer = nextBundleAnaluzer({
   enabled: !!process.env.ANALYZE,
 });
 
-module.exports = withBundleAnalyzer({
-  poweredByHeader: false,
-  publicRuntimeConfig: { localSubpaths },
-  rewrites: async () => nextI18NextRewrites(localSubpaths),
-  webpack: (config) => {
-    config.resolve.alias["~"] = path.resolve(__dirname, "src");
-    return config;
-  },
-});
+module.exports = withBundleAnalyzer(
+  withSourceMaps({
+    poweredByHeader: false,
+    publicRuntimeConfig: { localSubpaths },
+    rewrites: async () => nextI18NextRewrites(localSubpaths),
+    webpack: (config) => {
+      config.resolve.alias["~"] = path.resolve(__dirname, "src");
+      return config;
+    },
+  })
+);
