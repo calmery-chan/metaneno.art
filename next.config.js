d1771199ce4b62331d3d0399cf35ac665cd3df3f
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-10-27 01:47:11 +0900

    devtool を hidden-source-map にする

--- next.config.js
+++ next.config.js
@@ -21,9 +21,9 @@ const withBundleAnalyzer = nextBundleAnalyzer({
   enabled: !!process.env.ANALYZE,
 });
 
-const withSourceMaps = nextSourceMaps(/* {
+const withSourceMaps = nextSourceMaps({
   devtool: "hidden-source-map",
-} */);
+});
 
 const localSubpaths = {
   en: "en",
