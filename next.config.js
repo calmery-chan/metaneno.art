031944354c24702af8350f9ac4e39d067fedb701
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-10-27 03:10:31 +0900

    SENTRY_AUTH_TOKEN に関するコメントを追加する

--- next.config.js
+++ next.config.js
@@ -61,6 +61,11 @@ module.exports = withBundleAnalyzer(
             include: ".next",
             release: VERCEL_GITHUB_COMMIT_SHA,
             setCommits: {
+              // Sentry にある Vercel の Integration で追加される SENTRY_AUTH_TOKEN だとスコープの設定でデプロイに失敗してしまう
+              // error: API request failed
+              //   caused by: sentry reported an error: You do not have permission to perform this action. (http status: 403)
+              // https://sentry.io/settings/account/api/auth-tokens/ で認証トークンを作成、Vercel に登録する必要がある
+              // `event:read` と `event:write`、`org:integrations`、`org:read`、`org:write`、`project:read`、`project:releases`、`project:write` を追加した
               commit: VERCEL_GITHUB_COMMIT_SHA,
               repo: `${VERCEL_GITHUB_COMMIT_ORG}/${VERCEL_GITHUB_COMMIT_REPO}`,
             },
