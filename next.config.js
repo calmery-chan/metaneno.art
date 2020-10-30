6230b11b4e5fc50dd0aa63af7c6c30f881aaf4bf
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-10-30 18:18:51 +0900

    Sentry で GitHub のコミット情報を確認できるようにする

--- next.config.js
+++ next.config.js
@@ -6,14 +6,13 @@ const nextSourceMaps = require("@zeit/next-source-maps");
 // Environment Variables
 
 const {
+  GITHUB_REPOSITORY,
+  GITHUB_SHA,
   NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
   NEXT_PUBLIC_VERCEL_ENV: VERCEL_ENV,
   SENTRY_AUTH_TOKEN,
   SENTRY_ORG,
   SENTRY_PROJECT,
-  VERCEL_GITHUB_COMMIT_ORG,
-  VERCEL_GITHUB_COMMIT_REPO,
-  VERCEL_GITHUB_COMMIT_SHA,
 } = process.env;
 
 process.env.SENTRY_DSN = SENTRY_DSN;
@@ -39,14 +38,13 @@ module.exports = withBundleAnalyzer(
       }
 
       if (
+        GITHUB_REPOSITORY &&
+        GITHUB_SHA &&
         SENTRY_AUTH_TOKEN &&
         SENTRY_DSN &&
         SENTRY_ORG &&
         SENTRY_PROJECT &&
-        VERCEL_ENV &&
-        VERCEL_GITHUB_COMMIT_ORG &&
-        VERCEL_GITHUB_COMMIT_REPO &&
-        VERCEL_GITHUB_COMMIT_SHA
+        VERCEL_ENV
       ) {
         config.plugins.push(
           new SentryWebpackPlugin({
@@ -55,15 +53,15 @@ module.exports = withBundleAnalyzer(
             },
             ignore: ["node_modules"],
             include: ".next",
-            release: VERCEL_GITHUB_COMMIT_SHA,
+            release: GITHUB_SHA,
             setCommits: {
               // Sentry にある Vercel の Integration で追加される SENTRY_AUTH_TOKEN だとスコープの設定でデプロイに失敗してしまう
               // error: API request failed
               //   caused by: sentry reported an error: You do not have permission to perform this action. (http status: 403)
               // https://sentry.io/settings/account/api/auth-tokens/ で認証トークンを作成、Vercel に登録する必要がある
               // `event:read` と `event:write`、`org:integrations`、`org:read`、`org:write`、`project:read`、`project:releases`、`project:write` を追加した
-              commit: VERCEL_GITHUB_COMMIT_SHA,
-              repo: `${VERCEL_GITHUB_COMMIT_ORG}/${VERCEL_GITHUB_COMMIT_REPO}`,
+              commit: GITHUB_SHA,
+              repo: GITHUB_REPOSITORY,
             },
             stripPrefix: ["webpack://_N_E/"],
             urlPrefix: `~/_next`,
