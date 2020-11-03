dfa9a04100723e0defca59c7a1d97006d202e659
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-11-03 16:59:44 +0900

    画像の読み込みとリサイズ、@tensorflow-models/coco-ssd を使用した物体認識をして表示する

--- src/pages/noneme.tsx
+++ src/pages/noneme.tsx
@@ -1,8 +1,59 @@
+import "@tensorflow/tfjs";
+import * as cocoSsd from "@tensorflow-models/coco-ssd"; // eslint-disable-line import/no-unresolved
 import { NextPage } from "next";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  convertFileToImage,
+  convertImageToBlobUrl,
+  resizeImage,
+} from "~/utils/instax";
 
 const Noneme: NextPage = () => {
-  return <div />;
+  const [detectedObject, setDetectedObject] = useState<
+    cocoSsd.DetectedObject[]
+  >([]);
+  const [imageUrl, setImageUrl] = useState<string>();
+  const [model, setModel] = useState<cocoSsd.ObjectDetection>();
+
+  useEffect(() => {
+    (async () => {
+      setModel(await cocoSsd.load());
+    })();
+  }, []);
+
+  const handleOnChangeFile = useCallback(
+    async (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { files } = event.target;
+
+      if (!files || !files.length || !model) {
+        return;
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const image = await resizeImage(await convertFileToImage(files[0]))!;
+
+      setImageUrl(await convertImageToBlobUrl(image));
+      setDetectedObject(await model.detect(image));
+    },
+    [model]
+  );
+
+  if (!model) {
+    return null;
+  }
+
+  console.log(detectedObject);
+
+  return (
+    <>
+      <input
+        accept="image/jpeg,image/png"
+        onChange={handleOnChangeFile}
+        type="file"
+      />
+      {imageUrl && <img src={imageUrl} />}
+    </>
+  );
 };
 
 export default Noneme;
---
+++ src/utils/instax.ts
@@ -0,0 +1,82 @@
+import blueimpLoadImage from "blueimp-load-image";
+
+const IMAGE_MAX_WIDTH = 1200;
+const IMAGE_MAX_HEIGHT = 1200;
+
+// Helper Functions
+
+const convertUrlToImage = (url: string): Promise<HTMLImageElement> =>
+  new Promise((resolve, reject) => {
+    const image = new Image();
+
+    image.onerror = reject;
+    image.onload = () => resolve(image);
+
+    image.src = url;
+  });
+
+// Exports
+
+export const convertFileToImage = (file: File): Promise<HTMLImageElement> =>
+  new Promise((resolve, reject) => {
+    blueimpLoadImage(
+      file,
+      async (canvas) => {
+        if (canvas instanceof Event && canvas.type === "error") {
+          return reject(canvas);
+        }
+
+        resolve(
+          await convertUrlToImage(
+            (canvas as HTMLCanvasElement).toDataURL("image/png")
+          )
+        );
+      },
+      { canvas: true, orientation: true }
+    );
+  });
+
+export const convertImageToBlobUrl = (
+  image: HTMLImageElement
+): Promise<string> =>
+  new Promise((resolve) => {
+    const { width, height } = image;
+
+    const canvas = document.createElement("canvas");
+    canvas.height = height;
+    canvas.width = width;
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const context = canvas.getContext("2d")!;
+    context.drawImage(image, 0, 0, width, height);
+
+    canvas.toBlob((blob) => resolve(URL.createObjectURL(blob)), "image/png");
+  });
+
+export const resizeImage = async (image: HTMLImageElement) => {
+  const imageHeight = image.height;
+  const imageWidth = image.width;
+  const horizontalRatio = imageWidth / IMAGE_MAX_WIDTH;
+  const verticalRatio = imageHeight / IMAGE_MAX_HEIGHT;
+
+  let height = 0;
+  let width = 0;
+
+  if (horizontalRatio > verticalRatio) {
+    width = IMAGE_MAX_WIDTH;
+    height = imageHeight * (IMAGE_MAX_WIDTH / imageWidth);
+  } else {
+    width = imageWidth * (IMAGE_MAX_HEIGHT / imageHeight);
+    height = IMAGE_MAX_HEIGHT;
+  }
+
+  const canvas = document.createElement("canvas");
+  canvas.height = height;
+  canvas.width = width;
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const context = canvas.getContext("2d")!;
+  context.drawImage(image, 0, 0, width, height);
+
+  return await convertUrlToImage(canvas.toDataURL("image/png"));
+};
