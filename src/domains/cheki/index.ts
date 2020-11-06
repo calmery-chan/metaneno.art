adaf20b0b263a595f165433ab26f610f5e9bb716
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-11-07 03:57:40 +0900

    src/domains/cheki を追加する

---
+++ src/domains/cheki/index.ts
@@ -0,0 +1,5 @@
+import * as actions from "./actions";
+import * as selectors from "./selectors";
+
+export { actions, selectors };
+export { reducer } from "./reducer";
--- src/domains/index.ts
+++ src/domains/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { useDispatch as _useDispatch, createSelectorHook } from "react-redux";
+import * as cheki from "./cheki";
 import * as example from "./example";
 
 const reducer = combineReducers({
+  cheki: cheki.reducer,
   example: example.reducer,
 });
 
