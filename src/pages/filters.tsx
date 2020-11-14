7a60f5dcd535811776532e7ae1be37bbb7e1a368
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-11-14 22:37:42 +0900

    動作確認のための比較ページを追加する

---
+++ src/pages/filters.tsx
@@ -0,0 +1,313 @@
+import { NextPage } from "next";
+import React from "react";
+import {
+  A6,
+  ACG,
+  B5,
+  C1,
+  ChekiFilterDefs,
+  F2,
+  G3,
+  HB1,
+  HB2,
+  KK2,
+  LV3,
+  M3,
+  M5,
+  P5,
+  T1,
+  X1,
+} from "~/components/ChekiFilterDefs";
+
+const Filters: NextPage = () => {
+  return (
+    <div>
+      <div style={{ display: "flex" }}>
+        <div style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <div className="cssco cssco--c1" style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <svg
+          height="500"
+          viewBox="0 0 1000 1000"
+          width="500"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <ChekiFilterDefs />
+          <C1 height="1000" href="example.jpg" width="1000" />
+        </svg>
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <div style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <div className="cssco cssco--f2" style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <svg
+          height="500"
+          viewBox="0 0 1000 1000"
+          width="500"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <ChekiFilterDefs />
+          <F2 height="1000" href="example.jpg" width="1000" />
+        </svg>
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <div style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <div className="cssco cssco--g3" style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <svg
+          height="500"
+          viewBox="0 0 1000 1000"
+          width="500"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <ChekiFilterDefs />
+          <G3 height="1000" href="example.jpg" width="1000" />
+        </svg>
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <div style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <div className="cssco cssco--p5" style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <svg
+          height="500"
+          viewBox="0 0 1000 1000"
+          width="500"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <ChekiFilterDefs />
+          <P5 height="1000" href="example.jpg" width="1000" />
+        </svg>
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <div style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <div className="cssco cssco--hb1" style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <svg
+          height="500"
+          viewBox="0 0 1000 1000"
+          width="500"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <ChekiFilterDefs />
+          <HB1 height="1000" href="example.jpg" width="1000" />
+        </svg>
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <div style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <div className="cssco cssco--hb2" style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <svg
+          height="500"
+          viewBox="0 0 1000 1000"
+          width="500"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <ChekiFilterDefs />
+          <HB2 height="1000" href="example.jpg" width="1000" />
+        </svg>
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <div style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <div className="cssco cssco--acg" style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <svg
+          height="500"
+          viewBox="0 0 1000 1000"
+          width="500"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <ChekiFilterDefs />
+          <ACG height="1000" href="example.jpg" width="1000" />
+        </svg>
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <div style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <div className="cssco cssco--lv3" style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <svg
+          height="500"
+          viewBox="0 0 1000 1000"
+          width="500"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <ChekiFilterDefs />
+          <LV3 height="1000" href="example.jpg" width="1000" />
+        </svg>
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <div style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <div className="cssco cssco--m5" style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <svg
+          height="500"
+          viewBox="0 0 1000 1000"
+          width="500"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <ChekiFilterDefs />
+          <M5 height="1000" href="example.jpg" width="1000" />
+        </svg>
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <div style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <div className="cssco cssco--a6" style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <svg
+          height="500"
+          viewBox="0 0 1000 1000"
+          width="500"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <ChekiFilterDefs />
+          <A6 height="1000" href="example.jpg" width="1000" />
+        </svg>
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <div style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <div className="cssco cssco--kk2" style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <svg
+          height="500"
+          viewBox="0 0 1000 1000"
+          width="500"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <ChekiFilterDefs />
+          <KK2 height="1000" href="example.jpg" width="1000" />
+        </svg>
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <div style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <div className="cssco cssco--m3" style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <svg
+          height="500"
+          viewBox="0 0 1000 1000"
+          width="500"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <ChekiFilterDefs />
+          <M3 height="1000" href="example.jpg" width="1000" />
+        </svg>
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <div style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <div className="cssco cssco--t1" style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <svg
+          height="500"
+          viewBox="0 0 1000 1000"
+          width="500"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <ChekiFilterDefs />
+          <T1 height="1000" href="example.jpg" width="1000" />
+        </svg>
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <div style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <div className="cssco cssco--b5" style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <svg
+          height="500"
+          viewBox="0 0 1000 1000"
+          width="500"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <ChekiFilterDefs />
+          <B5 height="1000" href="example.jpg" width="1000" />
+        </svg>
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <div style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <div className="cssco cssco--x1" style={{ width: 500 }}>
+          <img src="example.jpg" />
+        </div>
+        <svg
+          height="500"
+          viewBox="0 0 1000 1000"
+          width="500"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+          <ChekiFilterDefs />
+          <X1 height="1000" href="example.jpg" width="1000" />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default Filters;
