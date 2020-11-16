f5905b3c1c93fa1ada636dc38651030934943f48
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-11-17 01:15:05 +0900

    ページのレイアウトで使用する定数などを追加する

---
+++ src/styles/mixin.ts
@@ -0,0 +1,23 @@
+import { css } from "linaria";
+
+const ANIMATION_DURATION = {
+  seconds: 0.4,
+  milliseconds: 400,
+};
+
+const animation = css`
+  transition: ease ${ANIMATION_DURATION.seconds}s;
+  animation-fill-mode: forwards;
+  animation-duration: ${ANIMATION_DURATION.seconds}s;
+`;
+
+const clickable = css`
+  cursor: pointer;
+  transition: ${ANIMATION_DURATION.seconds}s ease;
+
+  &:hover {
+    transform: scale(1.06);
+  }
+`;
+
+export const Mixin = { ANIMATION_DURATION, animation, clickable };
