99ff3a59a91a3c2908f4830ac972244f7a9e47fa
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-11-19 23:51:09 +0900

    チェキのフレームにグラデーションをかける

--- src/containers/Cheki/CanvasFrameLayer.tsx
+++ src/containers/Cheki/CanvasFrameLayer.tsx
@@ -28,6 +28,19 @@ export const ChekiCanvasFrameLayer: React.FC = () => {
         <rect fill="#fff" height={height} width={width} />
       </mask>
 
+      <defs>
+        <linearGradient
+          id="cheki-frame-layer-shadow"
+          x1="0"
+          y1="0"
+          x2="512"
+          y2="575"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="1" stopColor="#F5F5F5" />
+        </linearGradient>
+      </defs>
+
       {url && (
         <image
           height={CHEKI_VERTICAL_FRAME_HEIGHT}
@@ -36,6 +49,13 @@ export const ChekiCanvasFrameLayer: React.FC = () => {
           mask="url(#cheki-canvas-frame-layer)"
         />
       )}
+
+      <rect
+        fillOpacity="0.24"
+        height={CHEKI_VERTICAL_FRAME_HEIGHT}
+        width={CHEKI_HORIZONTAL_FRAME_WIDTH}
+        fill="url(#cheki-frame-layer-shadow)"
+      />
     </>
   );
 };
