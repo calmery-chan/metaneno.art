8d8ff00724e59aca22144bb9b703c62c9fba7378
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-11-19 18:49:12 +0900

    linaria 関連のパッケージと設定ファイルを削除する

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const nextBundleAnalyzer = require("@next/bundle-analyzer");
 const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 const nextSourceMaps = require("@zeit/next-source-maps");
-const withLinaria = require("next-linaria");
 
 // Environment Variables
 
@@ -30,51 +29,49 @@ const withSourceMaps = nextSourceMaps({
 });
 
 module.exports = withBundleAnalyzer(
-  withLinaria(
-    withSourceMaps({
-      poweredByHeader: false,
-      target: "serverless",
-      webpack: (config, options) => {
-        config.resolve.alias["~"] = path.resolve(__dirname, "src");
+  withSourceMaps({
+    poweredByHeader: false,
+    target: "serverless",
+    webpack: (config, options) => {
+      config.resolve.alias["~"] = path.resolve(__dirname, "src");
 
-        if (!options.isServer) {
-          config.resolve.alias["@sentry/node"] = "@sentry/browser";
-        }
+      if (!options.isServer) {
+        config.resolve.alias["@sentry/node"] = "@sentry/browser";
+      }
 
-        if (
-          GITHUB_REPOSITORY &&
-          GITHUB_SHA &&
-          SENTRY_AUTH_TOKEN &&
-          SENTRY_DSN &&
-          SENTRY_ORG &&
-          SENTRY_PROJECT &&
-          VERCEL_ENV
-        ) {
-          config.plugins.push(
-            new SentryWebpackPlugin({
-              deploy: {
-                env: VERCEL_ENV,
-              },
-              ignore: ["node_modules"],
-              include: ".next",
-              release: GITHUB_RELEASE_TAG_NAME || GITHUB_SHA,
-              setCommits: {
-                // Sentry にある Vercel の Integration で追加される SENTRY_AUTH_TOKEN だとスコープの設定でデプロイに失敗してしまう
-                // error: API request failed
-                //   caused by: sentry reported an error: You do not have permission to perform this action. (http status: 403)
-                // https://sentry.io/settings/account/api/auth-tokens/ で認証トークンを作成、Vercel に登録する必要がある
-                // `event:read` と `event:write`、`org:integrations`、`org:read`、`org:write`、`project:read`、`project:releases`、`project:write` を追加した
-                commit: GITHUB_SHA,
-                repo: GITHUB_REPOSITORY,
-              },
-              stripPrefix: ["webpack://_N_E/"],
-              urlPrefix: `~/_next`,
-            })
-          );
-        }
+      if (
+        GITHUB_REPOSITORY &&
+        GITHUB_SHA &&
+        SENTRY_AUTH_TOKEN &&
+        SENTRY_DSN &&
+        SENTRY_ORG &&
+        SENTRY_PROJECT &&
+        VERCEL_ENV
+      ) {
+        config.plugins.push(
+          new SentryWebpackPlugin({
+            deploy: {
+              env: VERCEL_ENV,
+            },
+            ignore: ["node_modules"],
+            include: ".next",
+            release: GITHUB_RELEASE_TAG_NAME || GITHUB_SHA,
+            setCommits: {
+              // Sentry にある Vercel の Integration で追加される SENTRY_AUTH_TOKEN だとスコープの設定でデプロイに失敗してしまう
+              // error: API request failed
+              //   caused by: sentry reported an error: You do not have permission to perform this action. (http status: 403)
+              // https://sentry.io/settings/account/api/auth-tokens/ で認証トークンを作成、Vercel に登録する必要がある
+              // `event:read` と `event:write`、`org:integrations`、`org:read`、`org:write`、`project:read`、`project:releases`、`project:write` を追加した
+              commit: GITHUB_SHA,
+              repo: GITHUB_REPOSITORY,
+            },
+            stripPrefix: ["webpack://_N_E/"],
+            urlPrefix: `~/_next`,
+          })
+        );
+      }
 
-        return config;
-      },
-    })
-  )
+      return config;
+    },
+  })
 );
