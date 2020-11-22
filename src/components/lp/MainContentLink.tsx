e81e2ad188db6fa8dd1104a34c5953c85eeb3122
yui540 <tabletennis540@gmail.com>
2020-11-22 12:41:10 +0900

    あらすじセクションの作成

---
+++ src/components/lp/MainContentLink.tsx
@@ -0,0 +1,200 @@
+import { keyframes, css } from "@emotion/react";
+import styled from "@emotion/styled";
+import React from "react";
+import { useIntersectionObserver } from "./hooks";
+import { colors, media } from "./variables";
+
+export default function MainContentLink() {
+  const [targetRef, isIntersected] = useIntersectionObserver<HTMLDivElement>(
+    {}
+  );
+
+  return (
+    <Wrapper ref={targetRef}>
+      <Bg isIntersected={isIntersected} />
+      <Inner>
+        <Message
+          isIntersected={isIntersected}
+          src="/lp/main-content-link/message.svg"
+        />
+        <ButtonWrapper>
+          <div>
+            <Ring isIntersected={isIntersected} src="/lp/decoration/ring.svg" />
+            <Ring isIntersected={isIntersected} src="/lp/decoration/ring.svg" />
+          </div>
+          <div>
+            <Triangle
+              isIntersected={isIntersected}
+              src="/lp/decoration/triangle.svg"
+            />
+            <Triangle
+              isIntersected={isIntersected}
+              src="/lp/decoration/triangle.svg"
+            />
+          </div>
+          <Button
+            src="/lp/main-content-link/coming-soon.svg"
+            isIntersected={isIntersected}
+          />
+        </ButtonWrapper>
+      </Inner>
+    </Wrapper>
+  );
+}
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  background-color: ${colors.yume};
+  width: 100%;
+`;
+
+const overray = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const Bg = styled.div<{ isIntersected: boolean }>`
+  ${overray}
+
+  &::before,
+  &::after {
+    content: "";
+    display: block;
+    ${overray}
+  }
+  &::before {
+    background: url("/lp/main-illust-min.jpg") top 30% center / cover no-repeat;
+    opacity: 0;
+
+    ${media.smallDown} {
+      background-position-x: 80%;
+    }
+
+    ${({ isIntersected }) =>
+      isIntersected &&
+      css`
+        animation: ${fadeIn} 1.6s ease-out 0s forwards;
+      `}
+  }
+  &::after {
+    background: linear-gradient(
+      to bottom,
+      ${colors.yume},
+      rgba(34, 57, 99, 0.3),
+      ${colors.yume}
+    );
+  }
+`;
+
+const Inner = styled.div`
+  position: relative;
+  display: grid;
+  justify-items: center;
+  padding: 100px 0;
+`;
+
+const Message = styled.img<{ isIntersected: boolean }>`
+  display: block;
+  height: 90px;
+  opacity: 0;
+
+  ${({ isIntersected }) =>
+    isIntersected &&
+    css`
+      animation: ${fadeIn} 0.6s ease-out 0.15s forwards;
+    `}
+`;
+
+const popin = keyframes`
+  from {
+    opacity: 0;
+  }
+  50%,
+  to {
+    opacity: 1;
+  }
+
+  from {
+    transform: translateY(18px);
+  }
+  50% {
+    transform: translateY(-6px);
+  }
+  to {
+    transform: translateY(0);
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  position: relative;
+`;
+
+const Button = styled.img<{ isIntersected: boolean }>`
+  position: relative;
+  display: block;
+  height: 180px;
+  opacity: 0;
+
+  ${({ isIntersected }) =>
+    isIntersected &&
+    css`
+      animation: ${popin} 0.5s ease-out 0.3s forwards;
+    `}
+`;
+
+const Ring = styled.img<{ isIntersected: boolean }>`
+  display: block;
+  position: absolute;
+  width: 25px;
+  opacity: 0;
+
+  &:first-of-type {
+    bottom: 36px;
+    left: 36px;
+  }
+  &:last-of-type {
+    top: 36px;
+    right: 36px;
+  }
+
+  ${({ isIntersected }) =>
+    isIntersected &&
+    css`
+      animation: ${fadeIn} 0.6s ease-out 0.3s forwards;
+    `}
+`;
+
+const Triangle = styled.img<{ isIntersected: boolean }>`
+  display: block;
+  position: absolute;
+  width: 25px;
+  opacity: 0;
+
+  &:first-of-type {
+    top: 36px;
+    left: 36px;
+  }
+  &:last-of-type {
+    bottom: 36px;
+    right: 36px;
+    transform: rotate(180deg);
+  }
+
+  ${({ isIntersected }) =>
+    isIntersected &&
+    css`
+      animation: ${fadeIn} 0.6s ease-out 0.3s forwards;
+    `}
+`;
---
+++ src/components/lp/Story.tsx
@@ -0,0 +1,206 @@
+import { keyframes, css } from "@emotion/react";
+import styled from "@emotion/styled";
+import React from "react";
+import Badge from "./Badge";
+import CreatorComment from "./CreatorComment";
+import MainContentLink from "./MainContentLink";
+import { useIntersectionObserver } from "./hooks";
+import { colors, media } from "./variables";
+
+export default function Story() {
+  const [descriptionRef, isDescriptionIntersected] = useIntersectionObserver<
+    HTMLDivElement
+  >({});
+  const [subTitleRef, isSubTitleIntersected] = useIntersectionObserver<
+    HTMLImageElement
+  >({});
+
+  return (
+    <Wrapper>
+      <Badge />
+      <WaveWrapper>
+        <Wave1 src="/lp/story/wave1.svg" />
+        <Wave2 src="/lp/story/wave2.svg" />
+      </WaveWrapper>
+      <Title src="/lp/title/story.svg" />
+      <Description
+        ref={descriptionRef}
+        isIntersected={isDescriptionIntersected}
+      >
+        <span>テキストテキストテキストテキストテキストテキストテキスト</span>
+        <br />
+        <span>テキストテキストテキストテ</span>
+        <br />
+        <span>テキストテキストテキストテキストテキストテキスト</span>
+        <br />
+        <span>テキストテキストテキテキストテ</span>
+        <br />
+        <span>テキストテキストテキストテキスト</span>
+      </Description>
+      <div>
+        <SubTitle
+          src="/lp/sub-title/comment.svg"
+          ref={subTitleRef}
+          isIntersected={isSubTitleIntersected}
+        />
+        <CommentWrapper>
+          {Array.from(Array(3)).map((_, i) => (
+            <CreatorComment
+              key={i}
+              iconUrl="/lp/goods/sample-thumb.jpg"
+              name="めたねのおくすり"
+              comment="テキストテキストテキストテキストテキステキストテキストテキストテキストテキステキストテキストテキストテキストテキステキストテキストテキストテキストテキス"
+            />
+          ))}
+        </CommentWrapper>
+      </div>
+      <MainContentLink />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  position: relative;
+  display: grid;
+  grid-gap: 52px;
+  justify-items: center;
+  padding-top: 140px;
+  background-color: ${colors.yume};
+  z-index: 1;
+
+  ${media.smallDown} {
+    grid-gap: 30px;
+    padding-top: 180px;
+  }
+
+  &::before,
+  &::after {
+    content: "";
+    display: block;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 36px;
+    background: url(/lp/decoration/wave-yume.png) center / auto 100% repeat-x;
+    transform: translateY(-100%);
+
+    ${media.smallDown} {
+      height: 24px;
+    }
+  }
+  &::before {
+    top: 0;
+  }
+  &::after {
+    bottom: 0;
+    transform: translateY(100%) rotate(0.5turn);
+  }
+`;
+
+const WaveWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 50%;
+  width: 1420px;
+  height: 100%;
+  pointer-events: none;
+  transform: translateX(-50%);
+
+  ${media.smallDown} {
+    display: none;
+  }
+`;
+
+const Wave1 = styled.img`
+  position: absolute;
+  top: 231px;
+  left: 0;
+  width: 242px;
+`;
+
+const Wave2 = styled.img`
+  position: absolute;
+  top: 260px;
+  right: 0;
+  width: 222px;
+`;
+
+const Title = styled.img`
+  display: block;
+  width: 420px;
+
+  ${media.smallDown} {
+    width: 280px;
+  }
+`;
+
+const slideIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(18px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const Description = styled.div<{ isIntersected: boolean }>`
+  font-size: 30px;
+  line-height: 80px;
+  color: #fff;
+  opacity: 0;
+
+  ${media.mediumUp} {
+    text-align: center;
+    margin-bottom: 60px;
+  }
+
+  ${media.smallDown} {
+    font-size: 20px;
+    line-height: 58px;
+    padding: 0 24px;
+  }
+
+  span {
+    ${media.mediumUp} {
+      padding: 8px 16px;
+      background: linear-gradient(to right, #2babca 0% 50%, transparent 50%)
+        left / 200% 100% no-repeat;
+    }
+  }
+
+  ${({ isIntersected }) =>
+    isIntersected &&
+    css`
+      animation: ${slideIn} 0.7s cubic-bezier(0, 0, 0.17, 0.99) 0s forwards;
+    `}
+`;
+
+const SubTitle = styled.img<{ isIntersected: boolean }>`
+  display: block;
+  height: 58px;
+  margin: 0 auto 52px;
+  opacity: 0;
+
+  ${media.smallDown} {
+    height: 40px;
+    margin: 0 auto 42px;
+  }
+
+  ${({ isIntersected }) =>
+    isIntersected &&
+    css`
+      animation: ${slideIn} 0.7s cubic-bezier(0, 0, 0.17, 0.99) 0s forwards;
+    `}
+`;
+
+const CommentWrapper = styled.div`
+  display: grid;
+  grid-gap: 52px;
+
+  ${media.smallDown} {
+    grid-gap: 42px;
+    padding: 0 24px;
+  }
+`;
