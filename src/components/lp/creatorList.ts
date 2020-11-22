492d1ce9df0721dfc7a44e7178990840df9acd07
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-11-22 19:42:57 +0900

    制作者コメントを更新する

--- src/components/lp/creatorList.ts
+++ src/components/lp/creatorList.ts
@@ -10,14 +10,15 @@ const creatorList: {
       "めたねのおくすりの考える最強のかわいい世界創造中です。普段と違った見方で作品を楽しんでいただけるよう作り込んでいくので、この世界にトリップできる日を楽しみにしてください！",
   },
   {
+    iconUrl: "/lp/credit/calmery.jpg",
+    name: "Calmery",
+    comment:
+      "ノネメちゃんチェキや STYLY を使った AR など、開発周りのお手伝いさせていただいています！",
+  },
+  {
     iconUrl: "/lp/credit/noneme.jpg",
     name: "ノネメちゃん",
     comment: "よろしくね～",
-  },
-  {
-    iconUrl: "/lp/credit/calmery-chan.jpg",
-    name: "かるめりちゃん",
-    comment: "おはよ...",
   },
 ];
 
