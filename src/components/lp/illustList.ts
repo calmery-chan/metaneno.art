35d9e27e41069bf92d5ce33583d2c359d7612965
yui540 <tabletennis540@gmail.com>
2020-11-22 12:41:40 +0900

    データリスト類の追加

---
+++ src/components/lp/illustList.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    thumb: "/lp/illust/1.png",
+    link: "/lp/illust/1.png",
+  },
+  {
+    thumb: "/lp/illust/2.png",
+    link: "/lp/illust/2.png",
+  },
+  {
+    thumb: "/lp/illust/3.png",
+    link: "/lp/illust/3.png",
+  },
+  {
+    thumb: "/lp/illust/4.png",
+    link: "/lp/illust/4.png",
+  },
+  {
+    thumb: "/lp/illust/5.png",
+    link: "/lp/illust/5.png",
+  },
+  {
+    thumb: "/lp/illust/6.png",
+    link: "/lp/illust/6.png",
+  },
+];
