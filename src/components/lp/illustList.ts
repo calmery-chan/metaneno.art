1c6f80aa8975bd7d313f5fc2a7c3b1c018b07ee6
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-11-22 19:05:58 +0900

    イラスト一覧を更新する

--- src/components/lp/illustList.ts
+++ src/components/lp/illustList.ts
@@ -3,32 +3,32 @@ const illustList: {
   link: string;
 }[] = [
   {
-    thumb: "/lp/illust/1.min.png",
-    link: "/lp/illust/1.png",
+    thumb: "/lp/illust/1.png",
+    link: "https://www.pixiv.net/artworks/82185561",
   },
   {
-    thumb: "/lp/illust/2.min.png",
-    link: "/lp/illust/2.png",
+    thumb: "/lp/illust/2.png",
+    link: "https://www.pixiv.net/artworks/80237493",
   },
   {
-    thumb: "/lp/illust/3.min.png",
-    link: "/lp/illust/3.png",
+    thumb: "/lp/illust/3.png",
+    link: "https://www.pixiv.net/artworks/78805173",
   },
   {
-    thumb: "/lp/illust/4.min.png",
-    link: "/lp/illust/4.png",
+    thumb: "/lp/illust/4.png",
+    link: "https://www.pixiv.net/artworks/80479147",
   },
   {
-    thumb: "/lp/illust/5.min.png",
-    link: "/lp/illust/5.png",
+    thumb: "/lp/illust/5.png",
+    link: "https://www.pixiv.net/artworks/84075833",
   },
   {
-    thumb: "/lp/illust/6.min.png",
-    link: "/lp/illust/6.png",
+    thumb: "/lp/illust/6.png",
+    link: "https://www.pixiv.net/artworks/78749032",
   },
   {
-    thumb: "/lp/illust/7.min.png",
-    link: "/lp/illust/7.png",
+    thumb: "/lp/illust/7.png",
+    link: "https://www.pixiv.net/artworks/83972014",
   },
 ];
 
