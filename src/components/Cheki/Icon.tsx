5b7b498c92ec26abee668d4e2fa87d12dd964456
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-12-07 01:28:04 +0900

    ESLint、SVGO を実行する

--- src/components/Cheki/Icon.tsx
+++ src/components/Cheki/Icon.tsx
@@ -2,10 +2,12 @@ import { css } from "@emotion/react";
 import React from "react";
 import { Mixin } from "~/styles/mixin";
 
-export const Icon: React.FC<React.DetailedHTMLProps<
-  React.ImgHTMLAttributes<HTMLImageElement>,
-  HTMLImageElement
->> = (props) => (
+export const Icon: React.FC<
+  React.DetailedHTMLProps<
+    React.ImgHTMLAttributes<HTMLImageElement>,
+    HTMLImageElement
+  >
+> = (props) => (
   <img
     css={css`
       ${Mixin.clickable};
