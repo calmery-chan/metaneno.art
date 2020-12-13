321b30672f20343a22961b8e63a4ef722e87f057
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-12-13 22:09:41 +0900

    decorations ページを追加する

---
+++ src/pages/cheki/decorations.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+import React from "react";
+import { ChekiApp } from "~/containers/Cheki/App";
+
+const Decorations: NextPage = () => <ChekiApp></ChekiApp>;
+
+export default Decorations;
