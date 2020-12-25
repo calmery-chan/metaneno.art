ecdfcc2af37c90f66d559cd1403a13a3aae8bc67
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-12-26 00:58:58 +0900

    Trim.tsx 関連のコンポーネントを 1 つのファイルにまとめる

--- src/containers/Cheki/Trim.tsx
+++ src/containers/Cheki/Trim.tsx
@@ -2,8 +2,6 @@ import { Theme } from "@emotion/react";
 import styled, { Interpolation } from "@emotion/styled";
 import React, { useCallback, useEffect, useRef } from "react";
 import { ChekiCanvasTrim } from "./CanvasTrim";
-import { ChekiTrimGrid } from "./TrimGrid";
-import { ChekiTrimImageOperator } from "./TrimImageOperator";
 import { useDispatch, useSelector, selectors } from "~/domains";
 import { actions } from "~/domains/cheki";
 import {
@@ -29,6 +27,195 @@ const ChekiTrimOverlay: React.FC = () => (
 );
 
 // Main
+
+const ChekiTrimImageOperator: React.FC = () => {
+  const dispatch = useDispatch();
+  const cheki = useSelector(selectors.cheki);
+  const { displayable, trim } = cheki.layout;
+
+  const handleOnStartDragging = useCallback(
+    (event: MouseRelatedEvent | TouchRelatedEvent) => {
+      dispatch(
+        actions.startImageDragging({
+          cursorPositions: convertEventToCursorPositions(event),
+        })
+      );
+    },
+    []
+  );
+
+  return (
+    <rect
+      fillOpacity="0"
+      height={trim.height}
+      onMouseDown={handleOnStartDragging}
+      onTouchStart={handleOnStartDragging}
+      style={{ cursor: "move" }}
+      width={trim.width}
+      x={trim.x - displayable.x}
+      y={trim.y - displayable.y}
+    />
+  );
+};
+
+const ChekiTrimGrid: React.FC = () => {
+  const cheki = useSelector(selectors.cheki);
+  const { displayable, trim } = cheki.layout;
+
+  return (
+    <svg
+      height={trim.height}
+      viewBox={`0 0 ${trim.viewBoxWidth} ${trim.viewBoxHeight}`}
+      width={trim.width}
+      x={trim.x - displayable.x}
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      y={trim.y - displayable.y}
+    >
+      <rect
+        fill="#fff"
+        fillOpacity="0.48"
+        height="100%"
+        width={1 * trim.displayMagnification}
+        x={trim.viewBoxWidth / 3}
+        y="0"
+      />
+
+      <rect
+        fill="#fff"
+        fillOpacity="0.48"
+        height="100%"
+        width={1 * trim.displayMagnification}
+        x={(trim.viewBoxWidth / 3) * 2}
+        y="0"
+      />
+
+      <rect
+        fill="#fff"
+        fillOpacity="0.48"
+        height={1 * trim.displayMagnification}
+        width="100%"
+        x="0"
+        y={trim.viewBoxHeight / 3}
+      />
+
+      <rect
+        fill="#fff"
+        fillOpacity="0.48"
+        height={1 * trim.displayMagnification}
+        width="100%"
+        x="0"
+        y={(trim.viewBoxHeight / 3) * 2}
+      />
+
+      {/* Border */}
+
+      <rect
+        fill="#fff"
+        fillOpacity="0.48"
+        height={1 * trim.displayMagnification}
+        width="100%"
+        x="0"
+        y="0"
+      />
+
+      <rect
+        fill="#fff"
+        fillOpacity="0.48"
+        height={1 * trim.displayMagnification}
+        width="100%"
+        x="0"
+        y={trim.viewBoxHeight - 1 * trim.displayMagnification}
+      />
+
+      <rect
+        fill="#fff"
+        fillOpacity="0.48"
+        height="100%"
+        width={1 * trim.displayMagnification}
+        x="0"
+        y="0"
+      />
+
+      <rect
+        fill="#fff"
+        fillOpacity="0.48"
+        height="100%"
+        width={1 * trim.displayMagnification}
+        x={trim.viewBoxWidth - 1 * trim.displayMagnification}
+        y="0"
+      />
+
+      {/* Top - Left */}
+
+      <rect
+        fill="#fff"
+        height={2 * trim.displayMagnification}
+        width={16 * trim.displayMagnification}
+        x="0"
+        y="0"
+      />
+      <rect
+        fill="#fff"
+        height={16 * trim.displayMagnification}
+        width={2 * trim.displayMagnification}
+        x="0"
+        y="0"
+      />
+
+      {/* Top - Right */}
+
+      <rect
+        fill="#fff"
+        height={2 * trim.displayMagnification}
+        width={16 * trim.displayMagnification}
+        x={trim.viewBoxWidth - 16 * trim.displayMagnification}
+        y="0"
+      />
+      <rect
+        fill="#fff"
+        height={16 * trim.displayMagnification}
+        width={2 * trim.displayMagnification}
+        x={trim.viewBoxWidth - 2 * trim.displayMagnification}
+        y="0"
+      />
+
+      {/* Bottom - Left */}
+
+      <rect
+        fill="#fff"
+        height={2 * trim.displayMagnification}
+        width={16 * trim.displayMagnification}
+        x="0"
+        y={trim.viewBoxHeight - 2 * trim.displayMagnification}
+      />
+      <rect
+        fill="#fff"
+        height={16 * trim.displayMagnification}
+        width={2 * trim.displayMagnification}
+        x="0"
+        y={trim.viewBoxHeight - 16 * trim.displayMagnification}
+      />
+
+      {/* Bottom - Right */}
+
+      <rect
+        fill="#fff"
+        height={2 * trim.displayMagnification}
+        width={16 * trim.displayMagnification}
+        x={trim.viewBoxWidth - 16 * trim.displayMagnification}
+        y={trim.viewBoxHeight - 2 * trim.displayMagnification}
+      />
+      <rect
+        fill="#fff"
+        height={16 * trim.displayMagnification}
+        width={2 * trim.displayMagnification}
+        x={trim.viewBoxWidth - 2 * trim.displayMagnification}
+        y={trim.viewBoxHeight - 16 * trim.displayMagnification}
+      />
+    </svg>
+  );
+};
 
 export const ChekiTrim: React.FC<{ emotion?: Interpolation<Theme> }> = ({
   emotion,
