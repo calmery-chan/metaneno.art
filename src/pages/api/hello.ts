44f584bed29f14ad77651d5335213c756be3e805
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-01-03 23:54:55 +0900

    hello.ts は不要であるので削除する

--- src/pages/api/hello.ts
+++
@@ -1,8 +0,0 @@
-import { NowRequest, NowResponse } from "@vercel/node";
-
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default (req: NowRequest, res: NowResponse): void => {
-  res.statusCode = 200;
-  res.json({ name: "John Doe" });
-};
