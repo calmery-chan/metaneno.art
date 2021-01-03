59963d56416ed2089805749cfc2ac21c14687da8
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-01-04 00:50:36 +0900

    Sentry のヘッダーの確認処理を削除する

--- src/pages/api/sentry-to-discord.ts
+++ src/pages/api/sentry-to-discord.ts
@@ -1,4 +1,3 @@
-import * as crypto from "crypto";
 import * as Sentry from "@sentry/node";
 import { NowRequest, NowResponse } from "@vercel/node";
 import axios from "axios";
@@ -7,28 +6,11 @@ if (process.env.NODE_ENV === "production") {
   Sentry.init({ dsn: process.env.NEXT_PUBLIC_SENTRY_DSN });
 }
 
-function checkSecretToken(request: NowRequest, secret = "") {
-  if (!process.env.WEBHOOK_SECRET) {
-    throw new Error("WEBHOOK_SECRET is not defined");
-  }
-
-  if (request.body === undefined) {
-    return false;
-  }
-
-  const digest = crypto
-    .createHmac("sha256", secret)
-    .update(JSON.stringify(request.body), "utf8")
-    .digest("hex");
-
-  return digest === request.headers["Sentry-Hook-Signature"];
-}
-
 export default async (
   request: NowRequest,
   response: NowResponse
 ): Promise<void> => {
-  if (request.method !== "POST" || !checkSecretToken(request)) {
+  if (request.method !== "POST") {
     response.statusCode = 400;
     response.end();
     return;
