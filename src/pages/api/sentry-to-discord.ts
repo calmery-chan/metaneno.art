06b8f226e793e1e8f294cf35d7f8c7028bef3b5d
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-01-04 00:05:37 +0900

    Sentry から呼び出される Webhook で Sentry-Hook-Signature を確認するように変更する

--- src/pages/api/sentry-to-discord.ts
+++ src/pages/api/sentry-to-discord.ts
@@ -1,3 +1,4 @@
+import * as crypto from "crypto";
 import * as Sentry from "@sentry/node";
 import { NowRequest, NowResponse } from "@vercel/node";
 import axios from "axios";
@@ -6,11 +7,28 @@ if (process.env.NODE_ENV === "production") {
   Sentry.init({ dsn: process.env.NEXT_PUBLIC_SENTRY_DSN });
 }
 
+function checkSecretToken(request: NowRequest, secret = "") {
+  if (!process.env.WEBHOOK_SECRET) {
+    throw new Error("WEBHOOK_SECRET is not defined");
+  }
+
+  if (request.body === undefined) {
+    return false;
+  }
+
+  const digest = crypto
+    .createHmac("sha256", secret)
+    .update(JSON.stringify(request.body), "utf8")
+    .digest("hex");
+
+  return digest === request.headers["Sentry-Hook-Signature"];
+}
+
 export default async (
   request: NowRequest,
   response: NowResponse
 ): Promise<void> => {
-  if (request.method !== "POST") {
+  if (request.method !== "POST" || !checkSecretToken(request)) {
     response.statusCode = 400;
     response.end();
     return;
