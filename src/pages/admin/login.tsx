362c61a2d64174c27b93f271262dff441e07dd1d
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-01-11 02:10:51 +0900

    reCAPTCHA の追加位置を変更する

--- src/pages/admin/login.tsx
+++ src/pages/admin/login.tsx
@@ -2,14 +2,11 @@ import { AxiosError } from "axios";
 import classNames from "classnames";
 import { useRouter } from "next/router";
 import React, { useCallback, useEffect, useState } from "react";
-import { GoogleReCaptcha } from "react-google-recaptcha-v3";
+import {
+  GoogleReCaptcha,
+  GoogleReCaptchaProvider,
+} from "react-google-recaptcha-v3";
 import axios from "~/utils/axios";
-
-const Page: React.FC = ({ children }) => (
-  <div className="bg-gray-100 w-full h-full">
-    <div className="container mx-auto py-8">{children}</div>
-  </div>
-);
 
 const AdminLogin: React.FC = () => {
   const router = useRouter();
@@ -62,74 +59,84 @@ const AdminLogin: React.FC = () => {
   }, []);
 
   return (
-    <Page>
-      <GoogleReCaptcha onVerify={handleOnChangeRecaptchaResponse} />
-      <div className="flex justify-center">
-        <div className="max-w-sm">
-          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Name
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
-                value={name}
-                onChange={handleOnChangeName}
-              />
+    <GoogleReCaptchaProvider
+      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+      scriptProps={{
+        async: true,
+        defer: true,
+      }}
+    >
+      <div className="bg-gray-100 w-full h-full">
+        <div className="container mx-auto py-8">
+          <GoogleReCaptcha onVerify={handleOnChangeRecaptchaResponse} />
+          <div className="flex justify-center">
+            <div className="max-w-sm">
+              <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Name
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    type="text"
+                    value={name}
+                    onChange={handleOnChangeName}
+                  />
+                </div>
+                <div className="mb-6">
+                  <label className="block text-gray-700 text-sm font-bold mb-2">
+                    Password
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    type="password"
+                    value={password}
+                    onChange={handleOnChangePassword}
+                  />
+                </div>
+                <div className="mb-6 flex items-center justify-between">
+                  <button
+                    onClick={handleOnClickSignInButton}
+                    className={classNames(
+                      "w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline",
+                      {
+                        "opacity-50 cursor-not-allowed": disabled,
+                      }
+                    )}
+                    disabled={disabled}
+                    type="button"
+                  >
+                    Sign In
+                  </button>
+                </div>
+                <div className="text-xs text-gray-500">
+                  This site is protected by reCAPTCHA and the Google{" "}
+                  <a
+                    href="https://policies.google.com/privacy"
+                    className="text-blue-500"
+                  >
+                    Privacy Policy
+                  </a>{" "}
+                  and{" "}
+                  <a
+                    href="https://policies.google.com/terms"
+                    className="text-blue-500"
+                  >
+                    Terms of Service
+                  </a>{" "}
+                  apply.
+                </div>
+              </form>
+              {errorMessage && (
+                <p className="text-red-500 text-xs text-center mt-4 italic">
+                  {errorMessage}
+                </p>
+              )}
             </div>
-            <div className="mb-6">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Password
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="password"
-                value={password}
-                onChange={handleOnChangePassword}
-              />
-            </div>
-            <div className="mb-6 flex items-center justify-between">
-              <button
-                onClick={handleOnClickSignInButton}
-                className={classNames(
-                  "w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline",
-                  {
-                    "opacity-50 cursor-not-allowed": disabled,
-                  }
-                )}
-                disabled={disabled}
-                type="button"
-              >
-                Sign In
-              </button>
-            </div>
-            <div className="text-xs text-gray-500">
-              This site is protected by reCAPTCHA and the Google{" "}
-              <a
-                href="https://policies.google.com/privacy"
-                className="text-blue-500"
-              >
-                Privacy Policy
-              </a>{" "}
-              and{" "}
-              <a
-                href="https://policies.google.com/terms"
-                className="text-blue-500"
-              >
-                Terms of Service
-              </a>{" "}
-              apply.
-            </div>
-          </form>
-          {errorMessage && (
-            <p className="text-red-500 text-xs text-center mt-4 italic">
-              {errorMessage}
-            </p>
-          )}
+          </div>
         </div>
       </div>
-    </Page>
+    </GoogleReCaptchaProvider>
   );
 };
 
