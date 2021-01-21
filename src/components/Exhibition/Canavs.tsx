1e39f7fd0ae706d061e9dd376c1695d7ebf46df9
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-01-22 03:05:44 +0900

    WIP

---
+++ src/components/Exhibition/Canavs.tsx
@@ -0,0 +1,22 @@
+import { VRM } from "@pixiv/three-vrm";
+import React, { useEffect, useState } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+const loadVrm = (url: string): Promise<VRM> =>
+  new Promise((resolve) =>
+    new GLTFLoader().load(url, (gltf) => VRM.from(gltf).then(resolve))
+  );
+
+export const ExhibitionCanavs: React.FC = React.memo(() => {
+  const [player, setPlayer] = useState<VRM | null>(null);
+
+  useEffect(() => {
+    (async () => setPlayer(await loadVrm("/exhibition/calmery-chan.vrm")))();
+  }, []);
+
+  if (!player) {
+    return null;
+  }
+
+  return <div />;
+});
