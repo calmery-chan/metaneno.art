595c824b2a4e35f2386874d73889dac256970649
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-02-02 05:25:38 +0900

    キャラクターが画面中央に移動するまで背景の位置を固定する

--- src/components/Exhibition/2d/Character.tsx
+++ src/components/Exhibition/2d/Character.tsx
@@ -6,13 +6,22 @@ import {
   EXHIBITION_2D_CHARACTER_WIDTH,
   EXHIBITION_2D_CHARACTER_Y,
   EXHIBITION_2D_CHARACTER_X,
+  EXHIBITION_2D_MINIMUM_STEP_TO_START_ANIMATION,
+  EXHIBITION_2D_DISTANCE_PER_STEP,
 } from "~/constants/exhibition";
 
 export const Exhibition2dCharacter = React.memo<{
   direction: "left" | "right";
   step: number;
 }>(({ direction, step }) => (
-  <svg x={EXHIBITION_2D_CHARACTER_X} y={EXHIBITION_2D_CHARACTER_Y}>
+  <svg
+    x={
+      step < EXHIBITION_2D_MINIMUM_STEP_TO_START_ANIMATION
+        ? step * EXHIBITION_2D_DISTANCE_PER_STEP
+        : EXHIBITION_2D_CHARACTER_X
+    }
+    y={EXHIBITION_2D_CHARACTER_Y}
+  >
     <image
       height={EXHIBITION_2D_CHARACTER_HEIGHT}
       style={{ imageRendering: "pixelated" }}
--- src/components/Exhibition/2d/Object.tsx
+++ src/components/Exhibition/2d/Object.tsx
@@ -1,4 +1,8 @@
-import { EXHIBITION_2D_HEIGHT } from "~/constants/exhibition";
+import {
+  EXHIBITION_2D_DISTANCE_PER_STEP,
+  EXHIBITION_2D_HEIGHT,
+  EXHIBITION_2D_MINIMUM_STEP_TO_START_ANIMATION,
+} from "~/constants/exhibition";
 
 export const Exhibition2dObject: React.FC<{
   speed?: number;
@@ -11,7 +15,14 @@ export const Exhibition2dObject: React.FC<{
     style={{
       imageRendering: "pixelated",
     }}
-    transform={`translate(${step * 4 * (speed || 1) * -1 + (x || 0)} 0)`}
+    transform={`translate(${
+      (step < EXHIBITION_2D_MINIMUM_STEP_TO_START_ANIMATION
+        ? 0
+        : (step - EXHIBITION_2D_MINIMUM_STEP_TO_START_ANIMATION) *
+          EXHIBITION_2D_DISTANCE_PER_STEP *
+          (speed || 1) *
+          -1) + (x || 0)
+    } 0)`}
     xlinkHref={url}
   />
 );
