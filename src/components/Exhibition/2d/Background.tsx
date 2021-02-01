bd258646f933fcbf7f3c805305bf73bee88e9341
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-02-02 04:02:36 +0900

    2D の背景を表示する Background コンポーネントのリファクタリングをする

--- src/components/Exhibition/2d/Background.tsx
+++ src/components/Exhibition/2d/Background.tsx
@@ -1,30 +1,26 @@
 import React from "react";
 import { EXHIBITION_2D_HEIGHT } from "~/constants/exhibition";
 
-const defaultProps: Partial<React.SVGProps<SVGImageElement>> = {
-  height: EXHIBITION_2D_HEIGHT,
-  style: {
-    imageRendering: "pixelated",
-  },
-};
+const Image: React.FC<{ speed?: number; step: number; url: string }> = ({
+  speed,
+  step,
+  url,
+}) => (
+  <image
+    height={EXHIBITION_2D_HEIGHT}
+    style={{
+      imageRendering: "pixelated",
+    }}
+    transform={`translate(-${step * 4 * (speed || 1)} 0)`}
+    xlinkHref={url}
+  />
+);
 
 export const Exhibition2dBackground: React.FC<{ step: number }> = React.memo(
-  ({ step }) => {
-    const position = step * 2;
-
-    return (
-      <>
-        <image
-          {...defaultProps}
-          transform={`translate(-${position < 0 ? 0 : position * 0.9} 0)`}
-          xlinkHref="/exhibition/background/0.png"
-        />
-        <image
-          {...defaultProps}
-          transform={`translate(-${position < 0 ? 0 : position} 0)`}
-          xlinkHref="/exhibition/background/1.png"
-        />
-      </>
-    );
-  }
+  ({ step }) => (
+    <>
+      <Image speed={0.9} step={step} url="/exhibition/background/0.png" />
+      <Image step={step} url="/exhibition/background/1.png" />
+    </>
+  )
 );
