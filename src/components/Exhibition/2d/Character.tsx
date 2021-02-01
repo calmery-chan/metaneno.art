9bb758a96183a67d954bb00736d03d6544164f1a
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-02-02 05:01:25 +0900

    キャラクターを画面中央に配置、向きを変更できるようにする

--- src/components/Exhibition/2d/Character.tsx
+++ src/components/Exhibition/2d/Character.tsx
@@ -2,28 +2,32 @@ import React from "react";
 import {
   EXHIBITION_2D_CHARACTER_ANIMATION_FRAME_COUNT,
   EXHIBITION_2D_CHARACTER_HEIGHT,
-  EXHIBITION_2D_CHARACTER_BOTTOM_OFFSET,
-  EXHIBITION_2D_HEIGHT,
   EXHIBITION_2D_CHARACTER_ANIMATION_PER_FRAME,
+  EXHIBITION_2D_CHARACTER_WIDTH,
+  EXHIBITION_2D_CHARACTER_Y,
+  EXHIBITION_2D_CHARACTER_X,
 } from "~/constants/exhibition";
 
 export const Exhibition2dCharacter: React.FC<{
   direction: "left" | "right";
   step: number;
 }> = React.memo(({ direction, step }) => (
-  <image
-    height={EXHIBITION_2D_CHARACTER_HEIGHT}
-    style={{ imageRendering: "pixelated" }}
-    xlinkHref={`/exhibition/character/${Math.floor(
-      (step %
-        (EXHIBITION_2D_CHARACTER_ANIMATION_FRAME_COUNT *
-          EXHIBITION_2D_CHARACTER_ANIMATION_PER_FRAME)) /
-        EXHIBITION_2D_CHARACTER_ANIMATION_PER_FRAME
-    )}.png`}
-    y={
-      EXHIBITION_2D_HEIGHT -
-      EXHIBITION_2D_CHARACTER_HEIGHT -
-      EXHIBITION_2D_CHARACTER_BOTTOM_OFFSET
-    }
-  />
+  <svg x={EXHIBITION_2D_CHARACTER_X} y={EXHIBITION_2D_CHARACTER_Y}>
+    <image
+      height={EXHIBITION_2D_CHARACTER_HEIGHT}
+      style={{ imageRendering: "pixelated" }}
+      transform={
+        direction === "left"
+          ? `scale(-1 1) translate(-${EXHIBITION_2D_CHARACTER_WIDTH} 0)`
+          : ""
+      }
+      width={EXHIBITION_2D_CHARACTER_WIDTH}
+      xlinkHref={`/exhibition/character/${Math.floor(
+        (step %
+          (EXHIBITION_2D_CHARACTER_ANIMATION_FRAME_COUNT *
+            EXHIBITION_2D_CHARACTER_ANIMATION_PER_FRAME)) /
+          EXHIBITION_2D_CHARACTER_ANIMATION_PER_FRAME
+      )}.png`}
+    />
+  </svg>
 ));
