77c33bf0f04f31f4093d488f16d3d10c64cade42
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-02-03 00:53:32 +0900

    背景画像など、移動スピードと初期位置を正しく適用できるように修正する

--- src/components/Exhibition/2d/Object.tsx
+++ src/components/Exhibition/2d/Object.tsx
@@ -5,6 +5,29 @@ import {
   EXHIBITION_2D_MOVING_DISTANCE_PER_STEP,
 } from "~/constants/exhibition";
 
+const getObjectX = (step: number) => {
+  // キャラクターが画面中央に存在しないとき、スクロールしない
+  if (step < EXHIBITION_2D_CHARACTER_HORIZONTAL_MARGIN_IN_STEP) {
+    return 0;
+  }
+
+  // step が背景のスクロール量の最大値を越えるとき、最大値で固定する
+  if (step > EXHIBITION_2D_BACKGROUND_MAX_STEP) {
+    return (
+      (EXHIBITION_2D_BACKGROUND_MAX_STEP -
+        EXHIBITION_2D_CHARACTER_HORIZONTAL_MARGIN_IN_STEP) *
+      EXHIBITION_2D_MOVING_DISTANCE_PER_STEP *
+      -1
+    );
+  }
+
+  return (
+    (step - EXHIBITION_2D_CHARACTER_HORIZONTAL_MARGIN_IN_STEP) *
+    EXHIBITION_2D_MOVING_DISTANCE_PER_STEP *
+    -1
+  );
+};
+
 export const Exhibition2dObject: React.FC<{
   speed?: number;
   step: number;
@@ -16,26 +39,7 @@ export const Exhibition2dObject: React.FC<{
     style={{
       imageRendering: "pixelated",
     }}
-    transform={`translate(-${(() => {
-      // キャラクターが画面中央に存在しないとき、スクロールしない
-      if (step < EXHIBITION_2D_CHARACTER_HORIZONTAL_MARGIN_IN_STEP) {
-        return 0;
-      }
-
-      // step が背景のスクロール量の最大値を越えるとき、最大値で固定する
-      if (step > EXHIBITION_2D_BACKGROUND_MAX_STEP) {
-        return (
-          (EXHIBITION_2D_BACKGROUND_MAX_STEP -
-            EXHIBITION_2D_CHARACTER_HORIZONTAL_MARGIN_IN_STEP) *
-          EXHIBITION_2D_MOVING_DISTANCE_PER_STEP
-        );
-      }
-
-      return (
-        (step - EXHIBITION_2D_CHARACTER_HORIZONTAL_MARGIN_IN_STEP) *
-        EXHIBITION_2D_MOVING_DISTANCE_PER_STEP
-      );
-    })()} 0)`}
+    transform={`translate(${getObjectX(step) * (speed || 1) + (x || 0)} 0)`}
     xlinkHref={url}
   />
 );
