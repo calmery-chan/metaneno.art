4c55d7d1cc72c730000a86dbc187c92b35e8711f
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-02-14 16:52:22 +0900

    カメラの処理を hooks にする

--- src/components/Exhibition/3d/Player.tsx
+++ src/components/Exhibition/3d/Player.tsx
@@ -7,31 +7,63 @@ import GLTFLoader from "three-gltf-loader";
 
 CameraControls.install({ THREE });
 
-export const Exhibition3dPlayer = React.memo<{
-  state: "running" | "standing" | "walking";
-}>(({ state }) => {
+const useCamera = (
+  position: Vector3 = new Vector3(0, 0, 0),
+  offset: Vector3 = new Vector3(0, 0, 0)
+) => {
   const [cameraControls, setCameraControls] = useState<CameraControls>();
-  const [animations, setAnimations] = useState<AnimationClip[]>();
-  const [mixer, setMixer] = useState<AnimationMixer>();
-  const [scene, setScene] = useState<Scene>();
-  const {
-    gl: { domElement },
-    camera,
-  } = useThree();
   const [clock, setClock] = useState<THREE.Clock>();
-  const [cameraCameraOffsetY, setCameraOffsetY] = useState(0);
+  const { camera, gl } = useThree();
 
   useEffect(() => {
     setClock(new THREE.Clock());
   }, []);
+
+  useEffect(() => {
+    const cameraControls = new CameraControls(camera, gl.domElement);
+
+    cameraControls.maxDistance = 10;
+    cameraControls.maxPolarAngle = 50 * (Math.PI / 180);
+    cameraControls.minDistance = 3;
+    cameraControls.minPolarAngle = 50 * (Math.PI / 180);
+    cameraControls.polarAngle = 50 * (Math.PI / 180);
+
+    setCameraControls(cameraControls);
+  }, [camera, gl.domElement]);
+
+  useFrame((_, delta) => {
+    if (clock && cameraControls) {
+      cameraControls.moveTo(
+        position.x + offset.x,
+        position.y + offset.y,
+        position.z + offset.z,
+        false
+      );
+
+      cameraControls.update(delta);
+    }
+  });
+};
+
+export const Exhibition3dPlayer = React.memo<{
+  state: "running" | "standing" | "walking";
+}>(({ state }) => {
+  const [animations, setAnimations] = useState<AnimationClip[]>();
+  const [mixer, setMixer] = useState<AnimationMixer>();
+  const [scene, setScene] = useState<Scene>();
+  const [cameraOffset, setCameraOffset] = useState<Vector3>();
 
   // Side Effects
 
   useEffect(() => {
     new GLTFLoader().load("/hikikomori_chan.glb", ({ animations, scene }) => {
       setAnimations(animations);
-      setCameraOffsetY(
-        new Box3().setFromObject(scene).getSize(new Vector3()).y / 2
+      setCameraOffset(
+        new Vector3(
+          0,
+          new Box3().setFromObject(scene).getSize(new Vector3()).y / 2,
+          0
+        )
       );
       setMixer(new AnimationMixer(scene));
       setScene(scene);
@@ -56,35 +88,13 @@ export const Exhibition3dPlayer = React.memo<{
     };
   }, [animations, mixer, state]);
 
-  useEffect(() => {
-    const cameraControls = new CameraControls(camera, domElement);
-
-    cameraControls.maxDistance = 10;
-    cameraControls.maxPolarAngle = 50 * (Math.PI / 180);
-    cameraControls.minDistance = 3;
-    cameraControls.minPolarAngle = 50 * (Math.PI / 180);
-    cameraControls.polarAngle = 50 * (Math.PI / 180);
-
-    setCameraControls(cameraControls);
-  }, [camera, domElement]);
+  useCamera(scene?.position, cameraOffset);
 
   useFrame((_, delta) => {
     /* Animation */
 
     if (mixer) {
       mixer.update(delta);
-    }
-
-    /* Camera */
-
-    if (clock && cameraControls && scene) {
-      cameraControls.moveTo(
-        scene.position.x,
-        scene.position.y + cameraCameraOffsetY,
-        scene.position.z,
-        false
-      );
-      cameraControls.update(delta);
     }
   });
 
