0478b3feb657b2625c0e64f7a39db7c82cfcf6dc
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-02-26 21:51:03 +0900

    エリアの指定を元に戻す

--- src/components/Exhibition/3d.tsx
+++ src/components/Exhibition/3d.tsx
@@ -66,7 +66,7 @@ export const Exhibition3d: React.FC<{
 }> = ({ creamsoda, examine, multiplay, onComplete, settings }) => {
   const defaultArea = creamsoda === "flower" ? "meadow" : "sea";
 
-  const [currentAreaName, setCurrentAreaName] = useState<AreaName>("cloud");
+  const [currentAreaName, setCurrentAreaName] = useState<AreaName>(defaultArea);
   const area = areas[currentAreaName];
   const [completed, setCompleted] = useState(false);
 
