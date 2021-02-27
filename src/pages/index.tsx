a7a1dd253b147ac20f4566d69c97df26e08ad908
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-02-27 11:30:19 +0900

    トップページのイラストに w-full を指定する

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,11 @@ import { NextPage } from "next";
 
 const Index: NextPage = () => (
   <div className="grid w-full h-full place-items-center">
-    <img src="/construction.png" style={{ maxWidth: `512px` }} />
+    <img
+      className="w-full"
+      src="/construction.png"
+      style={{ maxWidth: `512px` }}
+    />
   </div>
 );
 
